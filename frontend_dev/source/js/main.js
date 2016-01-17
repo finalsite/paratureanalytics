605c0cbb5c445594abc175a6fa4ad0e0e5fffd34
Joel Colucci <joelcolucci@gmail.com>
2016-01-16 21:54:15 -0500

    feat: add basic form handling ajax call

--- frontend_dev/source/js/main.js
+++ frontend_dev/source/js/main.js
@@ -1,3 +1,8 @@
+function getQueryParameters(str) {
+  // Source: https://css-tricks.com/snippets/jquery/get-query-params-object/
+  return str.replace(/(^\?)/,'').split("&").map(function(n){return n = n.split("="),this[n[0]] = n[1],this}.bind({}))[0]
+}
+
 function fetchData() {
   $.ajax({
     url: 'api/v1/action',
@@ -8,6 +13,7 @@ function fetchData() {
 
 
 function load(response) {
+  console.log(response);
   var htmlTemplate = transformData(response);
 
   // Append to DOM
@@ -21,10 +27,15 @@ function transformData(response) {
 
   rawData.forEach(function(elem) {
     var htmlElem = '<tr>';
-    htmlElem += '<td>' + elem.timestamp.slice(0, 10) + '</td>';
-    htmlElem += '<td>' + elem.ticketNumber + '</td>';
-    htmlElem += '<td>' + elem.assignedTo + '</td>';
-    htmlElem += '<td>' + elem.actionType + '</td>';
+    if (elem.ticketNumber) {
+      htmlElem += '<td>' + elem.timestamp.toLocaleString().slice(0, 10) + '</td>';
+      htmlElem += '<td>' + elem.ticketNumber + '</td>';
+      htmlElem += '<td>' + elem.assignedTo + '</td>';
+      htmlElem += '<td>' + elem.actionType + '</td>';
+    } else {
+      htmlElem += '<td>' + elem._id + '</td>';
+      htmlElem += '<td>' + elem.count + '</td>';
+    }
     htmlElem += '</tr>';
 
     htmlData.push(htmlElem);
@@ -35,4 +46,48 @@ function transformData(response) {
 }
 
 
-window.onload = fetchData;
+/* Event Listeners */
+
+$(function() {
+  $('#report').on('submit', function(event) {
+    event.preventDefault();
+
+    var data = $(this).serialize();
+    data = getQueryParameters(data);
+
+    if (data['groupBy'] === 'none') {
+      delete data['groupBy'];
+    }
+
+    if (data['dateMin'] === '') {
+      delete data['dateMin'];
+    }
+
+    if (data['dateMax'] === '') {
+      delete data['dateMax'];
+    }
+
+    if (data['actionType'] === '') {
+      delete data['actionType'];
+    }
+
+    if (data['ticketNumber'] === '') {
+      delete data['ticketNumber'];
+    }
+
+    if (data['assignedTo'] === 'all') {
+      delete data['assignedTo'];
+    }
+
+    if (data['assignedFrom'] === 'all') {
+      delete data['assignedFrom'];
+    }
+
+    var uri = '/api/v1/action?' + $.param(data)
+    $.ajax({
+      url: uri,
+      dataType: 'json',
+      success: load
+    });
+  })
+});
