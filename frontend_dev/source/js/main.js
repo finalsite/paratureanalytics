ca1f815b2375b84423280aab5c23f6d2d0f56cc9
Joel Colucci <joelcolucci@gmail.com>
2016-01-18 23:09:53 -0500

    feat: implement ajax load of assignedTo assignedFrom select inputs

--- frontend_dev/source/js/main.js
+++ frontend_dev/source/js/main.js
@@ -15,9 +15,27 @@ function loadActionTypeList() {
   });
 }
 
+function loadCsrList() {
+  $.ajax({
+    url: 'http://localhost:5000/api/v1/action/csr',
+    dataType: 'json',
+    success: function(response) {
+      var results = response.results;
 
+      var htmlTemplate = '<option value="all">All</option>';
+      for (var i = 0; i < results.length; i++) {
+        htmlTemplate += '<option value="' + results[i] + '">' + results[i] + '</option>';
+      }
+
+      $('#assignedTo').empty().append(htmlTemplate);
+      $('#assignedFrom').empty().append(htmlTemplate);
+    }
+  });
+}
+
+// Init app
 loadActionTypeList();
-
+loadCsrList();
 
 var requests = [];
 
@@ -32,12 +50,9 @@ $('#report').on('submit', function(event) {
   requests.push(request);
 });
 
-
 $('.tabs').on('click', '.tab__link', function() {
-  // Select all active class elements and remove them
   $('.tab__panel--active').removeClass('tab__panel--active');
 
-  // Apply active class to this element
   $selectedTabPanel = $(this).parent('.tab__panel');
   $selectedTabPanel.addClass('tab__panel--active');
 });
