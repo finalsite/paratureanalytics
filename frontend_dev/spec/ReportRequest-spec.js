32bd99bcbe8c162ae7d67562fea24c3b78d3020c
Joel Colucci <joelcolucci@gmail.com>
2016-01-18 21:14:20 -0500

    tests: add beforeAll afterAll

--- frontend_dev/spec/ReportRequest-spec.js
+++ frontend_dev/spec/ReportRequest-spec.js
@@ -1,20 +1,24 @@
 describe('ReportRequest', function() {
 
+  beforeAll(function() {
+    request = new ReportRequest('?actionType=all');
+  });
+
+  afterAll(function() {
+    request = null;
+  });
+
   it('raises exception when no argument provided', function() {
     expect(ReportRequest).toThrowError(Error);
   });
 
   it('_getQueryStrAsObj returns an object', function() {
-    var request = new ReportRequest('actionType=all');
-
     var result = request._getQueryStrAsObj('?actionType=1 Star Ticket Rating');
 
     expect(result).toEqual(jasmine.any(Object));
   });
 
   it('_parseParameters returns transformed query string', function() {
-    var request = new ReportRequest('?actionType=all');
-
     var result = request._parseParameters('?actionType=all&assignedTo=Bobby');
 
     expect(result).toEqual('assignedTo=Bobby');
