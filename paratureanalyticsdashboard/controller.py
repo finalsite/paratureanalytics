eda0b54b79161cbe0c0cd63d020d792556bebb42
Joel Colucci <joelcolucci@gmail.com>
2016-02-07 12:36:18 -0500

    refactor: make routes only serve static files no other logic

--- paratureanalyticsdashboard/controller.py
+++ paratureanalyticsdashboard/controller.py
@@ -2,8 +2,7 @@
 """
 
 
-from flask import Flask, render_template, request, redirect, jsonify
-
+from flask import Flask, render_template
 from . import app
 
 
@@ -12,6 +11,6 @@ def get_root():
     return render_template('index.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def handle_log():
+@app.route('/login', methods=['GET'])
+def get_login():
     return render_template('login.html')
