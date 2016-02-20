4a5d692f97f54e15788fa12ddf40f67710ce0d55
Joel Colucci <joelcolucci@gmail.com>
2016-02-20 11:27:24 -0500

    chore: update setup.py file with new package reqs w/ api merge

--- setup.py
+++ setup.py
@@ -4,19 +4,22 @@ from setuptools import setup
 setup(
     name='paratureanalytics',
     version='0.5',
-    description='Single page application for viewing Parature reporting data.',
+    description='Application for viewing Parature reporting data.',
     url='https://github.com/joelcolucci/paratureanalytics',
     author='Joel Colucci',
     author_email='joelcolucci@gmail.com',
     license='MIT',
-    packages=['paratureanalytics'],
+    packages=['paratureanalytics', 'paratureanalytics.models'],
     install_requires=[
         "Flask==0.10.1",
+        "Flask-HTTPAuth==2.7.1",
         "itsdangerous==0.24",
         "Jinja2==2.8",
         "MarkupSafe==0.23",
+        "pymongo==3.2.1",
         "Werkzeug==0.11.3",
-        "wheel==0.24.0"
+        "wheel==0.24.0",
+        "XlsxWriter==0.8.4"
     ],
     package_data={
       'static': 'paratureanalytics/static/*',
