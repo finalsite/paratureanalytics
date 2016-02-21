425d31edfa2c1f83cdced700436e060841b2e9e3
Joel Colucci <joelcolucci@gmail.com>
2016-02-21 13:59:28 -0500

    refactor: reduce report.js to abstract base class

--- frontend_dev/build/js/report.js
+++ frontend_dev/build/js/report.js
@@ -1,6 +1,6 @@
 /**
  *
- *
+ * Report - Abstract base class for Report component
  * @param {string} parameters Query string of form input name/values
  * @constructor
  *
@@ -10,23 +10,13 @@ function Report(parameters) {
   if (!parameters) {
     throw new RootException('Missing argument. 1 expected, 0 given!');
   }
-  this.parameters = this._parseParameters(parameters);
+  this.parameters = this._getQueryStrAsObj(paremeters);
+  this.queryString = this._parseParameters(this.parameters);
   this.endpoint = API_HOSTNAME + 'api/v1/action?';
   this.response = null;
   this.requestError = null;
 }
 
-/**
- *
- *
- *
- *
- */
-
-Report.prototype.headerTemplates = {
-  'standard': '<tr><th>Date</th><th>Ticket</th><th>Action</th><th>Assigned To</th><th>Assigned From</th></tr>',
-  'aggregate': '<tr><th>Date</th><th>Total</th></tr>'
-}
 
 /**
  *
@@ -37,31 +27,29 @@ Report.prototype.headerTemplates = {
  */
 
 Report.prototype._parseParameters = function(parameters) {
-  data = this._getQueryStrAsObj(parameters);
-
-  if (data['groupBy'] === 'none') {
-    delete data['groupBy'];
+  if (parameters['groupBy'] === 'none') {
+    delete parameters['groupBy'];
   }
-  if (data['dateMin'] === '') {
-    delete data['dateMin'];
+  if (parameters['dateMin'] === '') {
+    delete parameters['dateMin'];
   }
-  if (data['dateMax'] === '') {
-    delete data['dateMax'];
+  if (parameters['dateMax'] === '') {
+    delete parameters['dateMax'];
   }
-  if (data['actionType'] === 'all') {
-    delete data['actionType'];
+  if (parameters['actionType'] === 'all') {
+    delete parameters['actionType'];
   }
-  if (data['ticketNumber'] === '') {
-    delete data['ticketNumber'];
+  if (parameters['ticketNumber'] === '') {
+    delete parameters['ticketNumber'];
   }
-  if (data['assignedTo'] === 'all') {
-    delete data['assignedTo'];
+  if (parameters['assignedTo'] === 'all') {
+    delete parameters['assignedTo'];
   }
-  if (data['assignedFrom'] === 'all') {
-    delete data['assignedFrom'];
+  if (parameters['assignedFrom'] === 'all') {
+    delete parameters['assignedFrom'];
   }
 
-  return $.param(data);
+  return $.param(parameters);
 }
 
 /**
@@ -85,20 +73,7 @@ Report.prototype._getQueryStrAsObj = function(str) {
  */
 
 Report.prototype.render = function() {
-  var uri = this.endpoint + this.parameters;
-  console.log(uri);
-  var authorizationHeaderValue = 'Basic ' + b64EncodeUnicode(sessionStorage.accessToken + ':');
-  $.ajax({
-    url: uri,
-    dataType: 'json',
-    headers: {
-      'Authorization': authorizationHeaderValue
-    },
-    crossDomain: true,
-    success: this.onSuccess,
-    error: this.onError,
-    context: this
-  });
+  throw new NotImplementedException("Render method not implemented. All sub classes require a render method.")
 }
 
 /**
@@ -109,19 +84,7 @@ Report.prototype.render = function() {
  */
 
 Report.prototype.onSuccess = function(response) {
-  this.response = response;
-  var htmlTemplate = this.getResponseAsStrTemplate();
-
-  var queryType = response['type'];
-  var headers = this.headerTemplates[queryType];
-
-  $dataPlaceholderContent = $('.placeholder__data .placeholder__content');
-  $dataPlaceholderContent.removeClass('placeholder__content--empty');
-  $dataPlaceholderContent.children('h3').remove();
-
-  $('#total').empty().append('<p>Total: ' + response['total'] + '<p>');
-  $('#headers').empty().append(headers);
-  $('#results').empty().append(htmlTemplate);
+  throw new NotImplementedException("onSuccess method not implemented. All sub classes require a onSuccess method.")
 }
 
 /**
@@ -132,43 +95,5 @@ Report.prototype.onSuccess = function(response) {
  */
 
 Report.prototype.onError = function(response) {
-  if (response.status === 403) {
-    alert('Login session expired!');
-    window.location.replace('/login');
-  }
-  this.requestError = response;
-  $dataPlaceholderContent = $('.placeholder__data .placeholder__content');
-  $dataPlaceholderContent.children('h3').text('Something went wrong...');
-}
-
-/**
- *
- *
- *
- *
- */
-
-Report.prototype.getResponseAsStrTemplate = function() {
-  var rawData = this.response['results'];
-  var htmlData = [];
-
-  rawData.forEach(function(elem) {
-    var htmlElem = '<tr>';
-    if (elem.ticketNumber) {
-      htmlElem += '<td>' + elem.timestamp.toLocaleString().slice(0, 10) + '</td>';
-      htmlElem += '<td>' + elem.ticketNumber + '</td>';
-      htmlElem += '<td>' + elem.actionType + '</td>';
-      htmlElem += '<td>' + elem.assignedTo + '</td>';
-      htmlElem += '<td>' + (elem.assignedFrom || '') + '</td>';
-    } else {
-      htmlElem += '<td>' + elem._id + '</td>';
-      htmlElem += '<td>' + elem.count + '</td>';
-    }
-    htmlElem += '</tr>';
-
-    htmlData.push(htmlElem);
-  });
-
-  var htmlTemplate = htmlData.join('');
-  return htmlTemplate;
+  throw new NotImplementedException("onError method not implemented. All sub classes require a onError method.");
 }
